cc24339b561863647afc9c854848ec6f9aa655f4
yjystyle <junyeong.joycity@gmail.com>
2020-04-20 20:17:16 +0900

    springboot ssl 세팅

---
+++ src/test/java/com/junyeong/study/sshtest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junyeong.study.sshtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
